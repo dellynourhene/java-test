fff02d51f23493ab176607a498d2fe5c8d7ce83c
nourouma <nourouma@gmail.com>
2026-04-28 11:18:20 +0100

    ajout de la multiplication dans A

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
 public class App {
-    public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println("Somme = " + (a + b));
-    }
+public static void main(String[] args) {
+int a = Integer.parseInt(args[0].trim());
+int b = Integer.parseInt(args[1].trim());
+System.out.println("Somme = " + (a + b));
+System.out.println("Produit = " + (a * b));
+}
 }
